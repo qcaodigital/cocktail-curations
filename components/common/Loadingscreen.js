a4fedc661454d60cb90357a38a276dd5f2627d4d
qcaodigital <qcao.digital@gmail.com>
2020-11-26 21:06:31 -0500

    update loading screen component

--- components/common/Loadingscreen.js
+++ components/common/Loadingscreen.js
@@ -1,22 +1,75 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import styles from './Loadingscreen.module.scss';
 import PropTypes from 'prop-types';
+import { motion, AnimatePresence } from 'framer-motion';
 
 Loadingscreen.propTypes = {
     turnOffLoading: PropTypes.func.isRequired
 }
 
-
 export default function Loadingscreen({ turnOffLoading }){
-    setTimeout(() => {
-        // turnOffLoading();
-    }, 2000);
+    const animationTime = 1.25;
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        if(!isLoading){
+            turnOffLoading();
+        }
+    }, [isLoading])
+
     return (
-        <div className={styles.Loadingscreen}>
-            <div className={styles.textContainer}>
-                <h2>Welcome To</h2>
-                <h2>Cocktail Curations</h2>
-            </div>
-        </div>
+        <AnimatePresence exitBeforeEnter>
+            {isLoading && 
+            <motion.div exit={{ opacity: 0 }} transition={{ delay: 1 }} className={styles.Loadingscreen}>
+                <div className={styles.textContainer}>
+                    <Line text='Welcome To' duration={animationTime}/>
+                    <Line text='Cocktail Curations' duration={animationTime} delay={animationTime} closeLoading={() => setIsLoading(false)}/>
+                </div>
+            </motion.div>}
+        </AnimatePresence>
+    )
+}
+
+export const Line = ({ text, duration, delay, closeLoading }) => {
+    const [textExit, setTextExit] = useState(false);
+
+    const wordVariant = {
+        animate: {
+            y: '-50%',
+            transition: {
+                duration: duration
+            }
+        },
+        initial: {
+            y: '100%',
+            x: '-50%',
+            opacity: 1
+        },
+        exit: {
+            y: '-150%'
+        }
+    }
+
+    return (
+        <AnimatePresence>
+            {!textExit && <motion.h2 
+                exit={{ y: '-150%'}} 
+                transition={{ 
+                    duration: duration,
+                    delay: delay * 1.25
+                }} 
+                animate={{ y: '-50%', opacity: 1 }} 
+                initial={{ y: '100%', x: '-50%', opacity: 0 }} 
+                onAnimationComplete={() => {
+                    setTimeout(() => {
+                        setTextExit(true);
+                        if(closeLoading){
+                            closeLoading();
+                        }
+                    }, delay ? 0 : duration * 250);
+                }}
+            >
+                {text}
+            </motion.h2>}
+        </AnimatePresence>
     )
 }
