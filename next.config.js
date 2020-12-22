7c3148fc377757da861d47e3d457d41c482f33d5
qcaodigital <qcao.digital@gmail.com>
2020-12-22 17:05:39 -0500

    remove mailgun api from repo

--- next.config.js
+++ next.config.js
@@ -1,6 +1,3 @@
 module.exports = {
-  env: {
-    MAILGUN_API_KEY: 'cc6f46508878790d9d269d7e52bc0d2c-e5da0167-f8022f95',
-    MAILGUN_DOMAIN: 'sandbox560d2691d9c94e19bc2fe373be971d73.mailgun.org'
-  }
+  
 }
