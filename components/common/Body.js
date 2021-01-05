dab9d82523a691ec9e3f8057f9a5cde731264ba5
qcaodigital <qcao.digital@gmail.com>
2021-01-05 00:32:10 -0500

    fix casing error

--- components/common/Body.js
+++ components/common/Body.js
@@ -7,9 +7,9 @@ import HamburgerMenu from './HamburgerMenu';
 import styles from './Body.module.scss';
 import { AnimatePresence } from 'framer-motion';
 import navListData from '../../data/navList';
-import useViewport from '../../custom_hooks/main_state/useViewport'
-import useIsHamburgerMenuOpen from '../../custom_hooks/main_state/useIsHamburgerMenuOpen'
-import useNavList from '../../custom_hooks/main_state/useNavList'
+import useViewport from '../../custom_hooks/main_state/useViewport';
+import useIsHamburgerMenuOpen from '../../custom_hooks/main_state/useIsHamburgerMenuOpen';
+import useNavList from '../../custom_hooks/main_state/useNavList';
 
 export default function Body({ children }){
     const router = useRouter();
