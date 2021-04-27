2d45cdf560d96ff10f3ec95c211eeb8fadae8c50
qcaodigital <qcao.digital@gmail.com>
2021-04-27 15:06:03 -0400

    re-enable loading screen

--- components/common/Body.js
+++ components/common/Body.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import { useRouter } from 'next/router';
-// import LoadingScreen from './LoadingScreen';
 import Nav from '../nav/Nav';
 import Footer from '../footer/Footer';
 import HamburgerMenu from './HamburgerMenu';
@@ -12,6 +11,7 @@ import useViewport from '../../custom_hooks/main_state/useViewport';
 import useIsHamburgerMenuOpen from '../../custom_hooks/main_state/useIsHamburgerMenuOpen';
 import useNavList from './../../custom_hooks/main_state/useNavList';
 import PropTypes from 'prop-types';
+import LoadingScreen from './LoadingScreen';
 
 Body.propTypes = {
 	children: PropTypes.element.isRequired,
@@ -22,7 +22,7 @@ export default function Body({ children }) {
 	const viewport = useViewport();
 	const [isHamburgerMenuOpen, setIsHamburgerMenuOpen] = useIsHamburgerMenuOpen(viewport);
 	const navList = useNavList(navListData, router);
-	const [loadComplete, setLoadComplete] = useState(true); //set to false to enable loading screen
+	const [loadComplete, setLoadComplete] = useState(false); //set to false to enable loading screen
 	const [navHeight, setNavHeight] = useState(120);
 	const [isNavAniComplete, setIsNavAniComplete] = useState(false);
 	const [popup, setPopup] = useState({ isOpen: false, content: {} });
@@ -43,7 +43,7 @@ export default function Body({ children }) {
 				className={`${styles.Body} ${isHamburgerMenuOpen && styles.HBMopen}`}
 				style={{ '--navHeight': `${navHeight}px` }}
 			>
-				{/* <LoadingScreen turnOffLoading={() => setLoadComplete(true)} /> */}
+				<LoadingScreen turnOffLoading={() => setLoadComplete(true)} />
 				<AnimatePresence>
 					{popup.isOpen && <PopUp key='popup' setPopup={setPopup} popup={popup} />}
 				</AnimatePresence>
